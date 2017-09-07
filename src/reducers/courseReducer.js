8f381e3830aba2f3b99659cac0f58dc6758d419b
Ashutosh Singh Rawat <singh.ashutoshrawat@gmail.com>
2017-09-08 00:35:57 +0530

    Action types constants

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,6 +1,8 @@
+import * as types from '../actions/actionTypes';
+
 export default function courseReducer(state = [], action) {
   switch (action.type) {
-    case 'CREATE_COURSE':
+    case types.CREATE_COURSE:
       // state.push(action.course); //States are immutable
       // return state;
 
